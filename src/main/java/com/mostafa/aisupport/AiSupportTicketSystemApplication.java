2e3be1cebedc6d510a33088ab74a853468db5157
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-04-20 15:50:47 +0200

    Initialize project structure with Maven, including wrapper scripts and configuration files

---
+++ src/main/java/com/mostafa/aisupport/AiSupportTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mostafa.aisupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSupportTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSupportTicketSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mostafa/aisupport/AiSupportTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafa.aisupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSupportTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
