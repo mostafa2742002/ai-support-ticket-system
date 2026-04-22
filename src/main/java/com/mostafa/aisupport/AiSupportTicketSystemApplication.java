06f141ac2c795ec5d2736433c7f12fa9b470190c
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-04-22 18:44:23 +0200

    Enable asynchronous processing in the application

--- src/main/java/com/mostafa/aisupport/AiSupportTicketSystemApplication.java
+++ src/main/java/com/mostafa/aisupport/AiSupportTicketSystemApplication.java
@@ -1,9 +1,11 @@
 package com.mostafa.aisupport;
 
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAsync
 public class AiSupportTicketSystemApplication {
 
 	public static void main(String[] args) {
