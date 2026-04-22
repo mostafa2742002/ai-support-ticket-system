260be7dd60ef2231bed924a7924814b08260b356
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-04-22 15:09:16 +0200

    Allow public access to actuator endpoints in security configuration

--- src/main/java/com/mostafa/aisupport/common/security/SecurityConfig.java
+++ src/main/java/com/mostafa/aisupport/common/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/tickets").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/actuator","/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
