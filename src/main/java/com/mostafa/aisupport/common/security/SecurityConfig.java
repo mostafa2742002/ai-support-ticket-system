5caaf82819b2a9d0f22ad39cadf82d84d1b2c21a
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-04-22 19:42:13 +0200

    Add request for asking ticket status and update security configuration to permit access

--- src/main/java/com/mostafa/aisupport/common/security/SecurityConfig.java
+++ src/main/java/com/mostafa/aisupport/common/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/tickets").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/customer/tickets/ask").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/actuator","/actuator/**").permitAll()
                         .anyRequest().authenticated()
